ed32c3d1a7fbc2122ba7f5114a20dab7958c9d32
FagnerNunes <94726926+FagnerNunes@users.noreply.github.com>
2022-11-03 22:02:59 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const menuHamburguer = document.querySelector(".menu");
-menuHamburguer.addEventListener("click", () => {
-    const abrirMenu = document.querySelector("nav");
-    abrirMenu.classList.toggle("open")
-})
